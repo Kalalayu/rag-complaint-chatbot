af784e3e9fc41fd512e92506a8fe838dc36ed89f
KellyTroth16 <kalkidanalayu37@gmail.com>
2026-01-03 16:35:13 +0300

    Make Gradio chat interface runnable by fixing src.generator import and setting project root in sys.path

--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
 # app/main.py
+import sys
+import os
+
+# Add the project root to sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import gradio as gr
 from src.generator import generate_answer
 
